fdbdfe8500ce91d3f41610cc5a1fa4872b62fec2
Edoardo Sabadelli <edoardo@dhis2.org>
2022-07-25 15:44:20 +0200

    fix: avoid Visualization re-render when interpretations modal is toggled

--- src/components/App.js
+++ src/components/App.js
@@ -160,14 +160,13 @@ const App = () => {
             const isSaving = location.state?.isSaving
             const isOpening = location.state?.isOpening
             const isResetting = location.state?.isResetting
-            /*
             const isModalOpening = location.state?.isModalOpening
             const isModalClosing = location.state?.isModalClosing
             const isValidLocationChange =
                 previousLocation !== location.pathname &&
                 !isModalOpening &&
                 !isModalClosing
-*/
+
             if (
                 // currently editing
                 getVisualizationState(
@@ -185,7 +184,7 @@ const App = () => {
                     isSaving ||
                     isOpening ||
                     isResetting ||
-                    previousLocation !== location.pathname
+                    isValidLocationChange
                 ) {
                     loadVisualization(location)
                 }
